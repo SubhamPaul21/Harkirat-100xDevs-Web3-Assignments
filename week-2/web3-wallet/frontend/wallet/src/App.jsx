e9177341f0e7c79e8bc3d58194e6f50311c2621f
SubhamPaul21 <subhampaul12@gmail.com>
2024-08-23 08:49:49 +0530

    added header text and seed phrase box

--- week-2/web3-wallet/frontend/wallet/src/App.jsx
+++ week-2/web3-wallet/frontend/wallet/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import PhraseBox from './components/PhraseBox'
 
 function App() {
 
   return (
     <>
       <h2>Create Ethereum and Solana Wallets by Generating a Seed Phrase</h2>
+      <div className="seed-phrase-box">
+        <PhraseBox />
+      </div>
     </>
   )
 }
